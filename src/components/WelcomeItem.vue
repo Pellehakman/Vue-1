3e2d6deb603e07e382837a13e46383f2ae28eea6
Pellehakman <perhakman@outlook.com>
2022-12-16 08:50:29 +0100

    1

--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -1,7 +1,4 @@
 <template>
-  <div class="item">
-    <h4> {{ state.count}} x2 = {{ getters.times2}}</h4>
-    <button></button @click="inc(1)">
-  </div>
+  
 </template>
 
---
+++ src/views/HomeView/HomeView.vue
@@ -0,0 +1,7 @@
+<script lang="ts" src="./HomeView.ts"></script>
+
+<template>
+
+<h1>Hejf</h1>
+
+</template>
